7f18b5452b13f377b04d9bf9c8b5e51e6a9bf1ef
kent auyeung <kentayeung@gmail.com>
2021-04-06 17:24:51 -0700

    Adding comment test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
-
+// comment
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
