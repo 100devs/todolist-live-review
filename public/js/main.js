373f2b9cc7119d64a35e1d6e80efe1fdfa8d155c
Wolfgang-stack <wolfgang.a.mcrae@gmail.com>
2021-04-06 23:11:40 -0400

    Added comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,45 @@
+//assign a nodeList for delete buttons
 const deleteBtn = document.querySelectorAll('.del')
+//assign a nodeList for uncompleted todos
 const todoItem = document.querySelectorAll('.todoItem span')
+//assign a nodeList for completed todos
 const todoComplete = document.querySelectorAll('.todoItem span.completed')
-
+//add event listeners to all deleteBtns
 Array.from(deleteBtn).forEach((el)=>{
     el.addEventListener('click', deleteTodo)
 })
-
+//add event listeners to all uncompleted todos
 Array.from(todoItem).forEach((el)=>{
     el.addEventListener('click', markComplete)
 })
-
+//add event listeners to all completed todos
 Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', undo)
 })
 
+
+
+//define an async function to delete todos
 async function deleteTodo(){
+  //grab the inner text from the first child (span) of the container (li) 
     const todoText = this.parentNode.childNodes[1].innerText
     try{
+      //try a fetch (it's never gonna happennnnnn!)
+      //send a DELETE request to the server
         const response = await fetch('deleteTodo', {
+          //specify the method
             method: 'delete',
+            //specify the header (json)
             headers: {'Content-type': 'application/json'},
+            //specify the body - set it to the innerText grabbed from the span
             body: JSON.stringify({
                 'rainbowUnicorn': todoText
             })
         })
+        //receive and log the response
         const data = await response.json()
         console.log(data)
+        //reload the page
         location.reload()
     }catch(err){
         console.log(err)
@@ -33,23 +47,30 @@ async function deleteTodo(){
 }
 
 async function markComplete(){
+  //grab the inner text from the first child (span) of the container (li) 
     const todoText = this.parentNode.childNodes[1].innerText
     try{
+      //send a PUT request to the server
         const response = await fetch('markComplete', {
+          //specify the method
             method: 'put',
+            //headers
             headers: {'Content-type': 'application/json'},
+            //and the body - setting it to the text grabbed before
             body: JSON.stringify({
                 'rainbowUnicorn': todoText
             })
         })
+        //recieve and log the response data
         const data = await response.json()
         console.log(data)
+        //reload the page
         location.reload()
     }catch(err){
         console.log(err)
     }
 }
-
+//same as ^^^ but reversed - this will only run onClick of todos that are marked as completed
 async function undo(){
     const todoText = this.parentNode.childNodes[1].innerText
     try{
--- server.js
+++ server.js
@@ -1,74 +1,116 @@
+//Import express
 const express = require('express')
+//Initialize an instance of express()
+//this will be our server
 const app = express()
+//Import the MongoClient
 const MongoClient = require('mongodb').MongoClient
+//define the port that the server will listen to
 const PORT = 2121
+//import dotenv
 require('dotenv').config()
+//using .config() will parse the file and assign its contents process.env - returning an object
 
+//declare the db variable
 let db,
+//declare dbConnectionString and assign it to our dbURI saved in the .env file
     dbConnectionStr = process.env.DB_STRING,
+    //declare and assign the dbName: this is what our documents will be saved under
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+//Connect to the database
+MongoClient.connect(dbConnectionStr, 
+//avoid a deprecation warning
+{useUnifiedTopology: true})
     .then(client => {
+      //console.log on connection
         console.log(`Hey, connected to ${dbName} database`)
+        //assign the db
         db = client.db(dbName)
     })
     .catch(err =>{
         console.log(err)
     })
-
+//tell express we're using ejs
 app.set('view engine', 'ejs')
+//tell express where our static files are (css, js)
 app.use(express.static('public'))
+//allow express to recieve form data in request.body
 app.use(express.urlencoded({ extended: true }))
+//allows express to parse incoming json data
 app.use(express.json())
 
+//request handler for GET '/'
 app.get('/', async (req,res)=>{
+  //assign a variable to the data recieved in the collection as an array using MongoClient 
     const todoItems = await db.collection('todos').find().toArray()
+    //assign a variable to the items with the completed attribute equal to false using MongoClient 
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    //render the index.ejs file and pass through the data recieved
     res.render('index.ejs', {zebra: todoItems, left: itemsLeft})
 })
 
+
+//request handler for POST '/createTodo
 app.post('/createTodo', (req, res)=>{
+  //use MongoClient to insert a new document to the collection
+  //req.body.todoItem will be the form data received
+  //and we set the completed attribute to false
     db.collection('todos').insertOne({todo: req.body.todoItem, completed: false})
     .then(result =>{
+      //on save console.log confirmation and redirect to the home page
         console.log('Todo has been added!')
         res.redirect('/')
     })
 })
 
+//request handler for PUT '/markComplete'
 app.put('/markComplete', (req, res)=>{
+  //use MongoClient to find and update a specific document
     db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
+      //set the completed property to true
         $set: {
             completed: true
         }
     })
     .then(result =>{
+      
         console.log('Marked Complete')
+        //send a json response to the client
         res.json('Marked Complete')
     })
 })
 
+//request handler for PUT '/undo'
 app.put('/undo', (req, res)=>{
+  //use MongoClient to find and update a specific document
     db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
+        //set completed to false
         $set: {
             completed: false
         }
     })
     .then(result =>{
         console.log('Marked Complete')
+        //send a json response to the client
         res.json('Marked Complete')
     })
 })
 
+
+//request handler for DELETE '/deleteTodo'
 app.delete('/deleteTodo', (req, res)=>{
+  //use MongoClient to find and delete a specific document
     db.collection('todos').deleteOne({todo:req.body.rainbowUnicorn})
     .then(result =>{
         console.log('Deleted Todo')
+        //send json response
         res.json('Deleted It')
     })
     .catch( err => console.log(err))
 })
- 
+//set the PORT our server will listen on
+//process.env.PORT is for heroku
 app.listen(process.env.PORT || PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
