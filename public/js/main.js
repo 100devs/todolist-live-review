bd15dc17200ab7d6532f1d30b0ccd8353ec46dba
Sidharth Soman <siddu.soman65@gmail.com>
2021-04-07 05:37:31 +0530

    added comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('.todoItem span')
 const todoComplete = document.querySelectorAll('.todoItem span.completed')
+//setting up our variables which will hold the relevent elements
 
 Array.from(deleteBtn).forEach((el)=>{
     el.addEventListener('click', deleteTodo)
@@ -14,29 +15,32 @@ Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', undo)
 })
 
+//setting up the respective event listeners for the relevant buttons/texts
+
 async function deleteTodo(){
-    const todoText = this.parentNode.childNodes[1].innerText
+    const todoText = this.parentNode.childNodes[1].innerText //store name of task in var
     try{
-        const response = await fetch('deleteTodo', {
-            method: 'delete',
-            headers: {'Content-type': 'application/json'},
+        const response = await fetch('deleteTodo', { //fetch request to relevant api link
+            method: 'delete', //specifying method
+            headers: {'Content-type': 'application/json'}, //specifying content type of application/json
             body: JSON.stringify({
-                'rainbowUnicorn': todoText
+                'rainbowUnicorn': todoText //sending in our body with name of text set to rainbowUnicorn
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() //awaiting our json response
+        console.log(data) //logging data
+        location.reload() //refresh page
     }catch(err){
-        console.log(err)
+        console.log(err) //catch errs and log
     }
 }
 
+//SAME THING AS ABOVE MOSTLY
 async function markComplete(){
     const todoText = this.parentNode.childNodes[1].innerText
     try{
         const response = await fetch('markComplete', {
-            method: 'put',
+            method: 'put', //specifying our PUT type of request
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
                 'rainbowUnicorn': todoText
@@ -49,7 +53,7 @@ async function markComplete(){
         console.log(err)
     }
 }
-
+//SAME THING AS ABOVE MOSTLY
 async function undo(){
     const todoText = this.parentNode.childNodes[1].innerText
     try{
