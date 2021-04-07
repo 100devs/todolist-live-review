49bf139965bcdd6ed8d262a7cbc0ded126267cd1
rob <robert_regan86@yahoo.com>
2021-04-07 09:46:35 -0400

    comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,14 @@
-const deleteBtn = document.querySelectorAll('.del')
-const todoItem = document.querySelectorAll('.todoItem span')
-const todoComplete = document.querySelectorAll('.todoItem span.completed')
+const deleteBtn = document.querySelectorAll('.del') //selects all things with class of 'del' and set that as deleteBtn
+const todoItem = document.querySelectorAll('.todoItem span') // same as above but looks for all things with class of 'todoItem' AND is a span
+const todoComplete = document.querySelectorAll('.todoItem span.completed') // looks for completed todos and put into variable todoComplete
 
+
+// this takes everything line 1 found, puts it into an array, and adds an event listener to each one.
 Array.from(deleteBtn).forEach((el)=>{
-    el.addEventListener('click', deleteTodo)
+    el.addEventListener('click', deleteTodo) // listening for a click and when it receives one fire deleteTodo function
 })
 
+// for each element that we have stored in todoItem, add a click event that will fire markComplete function.
 Array.from(todoItem).forEach((el)=>{
     el.addEventListener('click', markComplete)
 })
@@ -14,6 +17,7 @@ Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', undo)
 })
 
+// here is our delete function. basically what it is doing is it will take all the text, from the line the user clicked, and delete it. then it reloads the page to update our list. 
 async function deleteTodo(){
     const todoText = this.parentNode.childNodes[1].innerText
     try{
@@ -31,29 +35,29 @@ async function deleteTodo(){
         console.log(err)
     }
 }
-
+// this will allow us to grab whatever was clicked, and fetch our markComplete function from server.js which updates an item in our database, in this case it will set the completed property as true
 async function markComplete(){
-    const todoText = this.parentNode.childNodes[1].innerText
+    const todoText = this.parentNode.childNodes[1].innerText // 'this' keeps us on current thing clicked, the rest will select the text.
     try{
         const response = await fetch('markComplete', {
-            method: 'put',
+            method: 'put', // we are gonna change something in our db, need put method
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
-                'rainbowUnicorn': todoText
+                'rainbowUnicorn': todoText //gives the text grabbed the property of rainbowUnicorn
             })
         })
         const data = await response.json()
         console.log(data)
-        location.reload()
+        location.reload() // reload page to run our get request again
     }catch(err){
         console.log(err)
     }
 }
-
+// here is another put method that undo's what the above function does. 
 async function undo(){
     const todoText = this.parentNode.childNodes[1].innerText
     try{
-        const response = await fetch('undo', {
+        const response = await fetch('undo', { 
             method: 'put',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
