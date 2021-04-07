bfda97a0975265b3944c71ad3d5b7aaceb3fd3ba
meera-ramesh19 <meeraramesh19@outlook.com>
2021-04-07 14:20:40 -0400

    added comments to everyline in server.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,53 @@
 const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('.todoItem span')
 const todoComplete = document.querySelectorAll('.todoItem span.completed')
-
-Array.from(deleteBtn).forEach((el)=>{
+    //creates a new shallow-copied array instance
+Array.from(deleteBtn).forEach((el) => {
     el.addEventListener('click', deleteTodo)
 })
 
-Array.from(todoItem).forEach((el)=>{
+Array.from(todoItem).forEach((el) => {
     el.addEventListener('click', markComplete)
 })
 
-Array.from(todoComplete).forEach((el)=>{
-    el.addEventListener('click', undo)
-})
-
-async function deleteTodo(){
+Array.from(todoComplete).forEach((el) => {
+        el.addEventListener('click', undo)
+    })
+    // async enables asyncronous promise based behavior 
+    // as if it was synchronous but without blocking the main thread.
+    //this makes it more readable.this always returns a promise-resolved or rejected
+async function deleteTodo() {
     const todoText = this.parentNode.childNodes[1].innerText
-    try{
+    try {
+        //when you await a promise ,the function is paused in a nonblocking way
+        // so that the promise settles. await is used to wait for a promise to resolve 
+        //or reject which can only be inside of an async function
         const response = await fetch('deleteTodo', {
-            method: 'delete',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'rainbowUnicorn': todoText
+                method: 'delete',
+                //allos to perfom various actions on repsonse and request headers
+                // to retrive, set , add to and removing headers
+                headers: { 'Content-type': 'application/json' },
+                //converts a JS object into a JSON string
+                body: JSON.stringify({
+                    'rainbowUnicorn': todoText
+                })
             })
-        })
+            //result of taking JSON as input and parsing it to produce a JS object 
         const data = await response.json()
         console.log(data)
+            //reloads the current document
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markComplete(){
+async function markComplete() {
     const todoText = this.parentNode.childNodes[1].innerText
-    try{
+    try {
         const response = await fetch('markComplete', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'rainbowUnicorn': todoText
             })
@@ -45,17 +55,17 @@ async function markComplete(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function undo(){
+async function undo() {
     const todoText = this.parentNode.childNodes[1].innerText
-    try{
+    try {
         const response = await fetch('undo', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 'rainbowUnicorn': todoText
             })
@@ -63,7 +73,7 @@ async function undo(){
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
--- server.js
+++ server.js
@@ -1,74 +1,124 @@
+//require loads the modules and give the access
+//to their exports. It is stored in a variable which
+//can be any name but convention is to use the module name
 const express = require('express')
+    // calls the express application and store in variable app
+    //We can say it as a newly created object
 const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
 
+//connect to a database 
+const MongoClient = require('mongodb').MongoClient
+    // use the given port
+const PORT = 2121
+    //loads the dotenv modules, allows secrets about your
+    //database credentials to be stored and shared
+require('dotenv').config()
+    //store your mongodb database credentials
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
-
-MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+    //connection configuration
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    //promise resolve
     .then(client => {
         console.log(`Hey, connected to ${dbName} database`)
         db = client.db(dbName)
     })
-    .catch(err =>{
+    //promise unresolve display the error
+    .catch(err => {
         console.log(err)
     })
-
+    //set EJS as templating engine
 app.set('view engine', 'ejs')
+    //adds a middleware to maek it possible to access 
+    //files from the public folder via HTTP
 app.use(express.static('public'))
+    // middleware that parses urlencoded with the querystring module
 app.use(express.urlencoded({ extended: true }))
+    //middleware that only parses JSON
 app.use(express.json())
+    // this function lets you define a route
+    //handler for GET requests to a given URL.express is 
+    //called when it receives an HTTP GET request to home dir /
+    // conver this function that uses async/await 
+app.get('/', async(req, res) => {
+        //we call an await inside of the request handler and returns a
+        //curosr to the documents that match the query criteria and converts into am array
+        // to the cursor returned from the find
+        const todoItems = await db.collection('todos').find().toArray()
+        const itemsLeft = await db.collection('todos').countDocuments({ completed: false })
 
-app.get('/', async (req,res)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    res.render('index.ejs', {zebra: todoItems, left: itemsLeft})
+        // render function is used to render a view and sends the HMTL string to the client without the middleware
+        // pass a local variable to the view
+        res.render('index.ejs', { zebra: todoItems, left: itemsLeft })
+    })
+    //user clicks on the button on HTML page, it will post request 
+    //to server and get the response
+app.post('/createTodo', (req, res) => {
+        //insert a single document into a collection if it exists
+        //else create the collection and insert the document into it.
+        db.collection('todos').insertOne({ todo: req.body.todoItem, completed: false })
+            //returns a promise
+            .then(result => {
+                console.log('Todo has been added!')
+                    //redirects the page to home
+                res.redirect('/')
+            })
+    })
+    //post and put methods are used to send data to the servers
+    //PUT is used to update the data 
+
+app.put('/markComplete', (req, res) => {
+    // updateOne in the database updates the first collection that matches 
+    //the specified filter.
+    db.collection('todos').updateOne({ todo: req.body.rainbowUnicorn }, {
+            //set is used to specify the name of each field to add and sets it value
+            // however if the name of the new field is the same as an existing field name,
+            // $set overwrites th value of that field with the value of the specified expression
+            $set: {
+                completed: true
+            }
+        })
+        //returns a promise
+        .then(result => {
+            console.log('Marked Complete')
+                // res.json sends a JSON  response. it is 
+                //identical to res.send() when an object or array is passed,
+                //but it also converts non-objects(null,undefined) to JSON
+            res.json('Marked Complete')
+        })
 })
 
-app.post('/createTodo', (req, res)=>{
-    db.collection('todos').insertOne({todo: req.body.todoItem, completed: false})
-    .then(result =>{
-        console.log('Todo has been added!')
-        res.redirect('/')
+app.put('/undo', (req, res) => {
+        db.collection('todos').updateOne({ todo: req.body.rainbowUnicorn }, {
+                $set: {
+                    completed: false
+                }
+            })
+            .then(result => {
+                console.log('Marked Complete')
+                res.json('Marked Complete')
+            })
     })
-})
+    //respond to a delete request to the /deleteTODO route
+    //to delete a serverside file via http delete method
+app.delete('/deleteTodo', (req, res) => {
+        //removes a single document form the collection
+        db.collection('todos').deleteOne({ todo: req.body.rainbowUnicorn })
+            //returns a promise
+            .then(result => {
+                console.log('Deleted Todo')
 
-app.put('/markComplete', (req, res)=>{
-    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
-        $set: {
-            completed: true
-        }
+                res.json('Deleted It')
+            })
+            //catch errors from the promise is rejected
+            .catch(err => console.log(err))
     })
-    .then(result =>{
-        console.log('Marked Complete')
-        res.json('Marked Complete')
-    })
-})
-
-app.put('/undo', (req, res)=>{
-    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
-        $set: {
-            completed: false
-        }
-    })
-    .then(result =>{
-        console.log('Marked Complete')
-        res.json('Marked Complete')
-    })
-})
-
-app.delete('/deleteTodo', (req, res)=>{
-    db.collection('todos').deleteOne({todo:req.body.rainbowUnicorn})
-    .then(result =>{
-        console.log('Deleted Todo')
-        res.json('Deleted It')
-    })
-    .catch( err => console.log(err))
-})
- 
-app.listen(process.env.PORT || PORT, ()=>{
+    //listens for the connections on the given path
+    // binds and listens for connections on the specified host and port
+    //process.env.port is used when hosting your app on another sevice,
+    //so they can configure the variable for you or you can 
+    //hardcode the PORT in your code
+app.listen(process.env.PORT || PORT, () => {
     console.log('Server is running, you better catch it!')
-})    
+})
