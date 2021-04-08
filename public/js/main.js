980c2ffde2420c41f6c644cd47fd93e40ff0f565
Ruth Reed <ruthreed.dev@gmail.com>
2021-04-08 15:20:22 -0600

    Commented every line except for when it got super repetitive and long in the package-lock.json.

--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,69 @@
-const deleteBtn = document.querySelectorAll('.del')
-const todoItem = document.querySelectorAll('.todoItem span')
-const todoComplete = document.querySelectorAll('.todoItem span.completed')
+const deleteBtn = document.querySelectorAll('.del')//assigning the deleteBtn value to be equivalent to the del class in the ejs.
+const todoItem = document.querySelectorAll('.todoItem span')//assigning the todoItem value to be equivalent to the .todoItem and span classes in the ejs.
+const todoComplete = document.querySelectorAll('.todoItem span.completed')//assigning the todoComplete value to be equivalent to the todoItem and span.completed classes in the ejs.
 
-Array.from(deleteBtn).forEach((el)=>{
-    el.addEventListener('click', deleteTodo)
+Array.from(deleteBtn).forEach((el)=>{//Making an array out of each of the items in the deleteBtn variable with an 'el' parameter. Not sure what 'el' stands for. 
+    el.addEventListener('click', deleteTodo)//Adding a click smurf to the 'el' value and setting up the deleteTodo function.
 })
 
-Array.from(todoItem).forEach((el)=>{
-    el.addEventListener('click', markComplete)
+Array.from(todoItem).forEach((el)=>{//Making an array out of each of the items in the todoItem variable with an 'el' parameter. 
+    el.addEventListener('click', markComplete)//Adding a click smurf to the 'el' value and setting up the markComplete function.
 })
 
-Array.from(todoComplete).forEach((el)=>{
-    el.addEventListener('click', undo)
+Array.from(todoComplete).forEach((el)=>{//Making an array out of each of the items in the todoComplete variable with an 'el' parameter.
+    el.addEventListener('click', undo)//Adding a click smurf to the 'el' value and setting up the undo function.
 })
 
-async function deleteTodo(){
-    const todoText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteTodo', {
-            method: 'delete',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'rainbowUnicorn': todoText
+async function deleteTodo(){//Starting the async function called deleteTodo()
+    const todoText = this.parentNode.childNodes[1].innerText//declaring the todoText variable. Not sure what any of it breaks down to be except for the 'innerText' bit.
+    try{//beginning of the try/catch function. Will happen first before the catch bit.
+        const response = await fetch('deleteTodo', {//Creating the response variable to be equal to waiting for the data in deleteTodo to be fetched.
+            method: 'delete',//declaring that the method is delete as opposed to the other methods in CRUD.
+            headers: {'Content-type': 'application/json'},//Declaring the headers to be JSON. Not sure what 'content-type' is and what could be put in its place.
+            body: JSON.stringify({//Putting the JSON into a string?
+                'rainbowUnicorn': todoText// making 'rainbowUnicorn' equal to todoText.
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+        const data = await response.json()//Declaring data to wait for the JSON response.
+        console.log(data)//Console logging the data variable.
+        location.reload()//Reloading the page? Not sure what location is.
+    }catch(err){//second part of the try/catch function. Runs if there is an error.
+        console.log(err)//Console logging the error if there is one.
     }
 }
 
-async function markComplete(){
-    const todoText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'rainbowUnicorn': todoText
+async function markComplete(){//Beginning the async function called markComplete().
+    const todoText = this.parentNode.childNodes[1].innerText//declaring the todoText variable. Not sure what any of it breaks down to be except for the 'innerText' bit.
+    try{//beginning of the try/catch function. Will happen first before the catch bit.
+        const response = await fetch('markComplete', {//Creating the response variable to be equal to waiting for the data in markComplete to be fetched.
+            method: 'put',//declaring that the method is put as opposed to the other methods in CRUD.
+            headers: {'Content-type': 'application/json'},//Declaring the headers to be JSON. Not sure what 'content-type' is and what could be put in its place.
+            body: JSON.stringify({//Putting the JSON into a string?
+                'rainbowUnicorn': todoText//making 'rainbowUnicorn' equal to todoText.
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+        const data = await response.json()//Declaring data to wait for the JSON response.
+        console.log(data)//Console logging the data variable.
+        location.reload()//Reloading the page? Not sure what location is.
+    }catch(err){//second part of the try/catch function. Runs if there is an error.
+        console.log(err)//Console logging the error if there is one.
     }
 }
 
-async function undo(){
-    const todoText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('undo', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'rainbowUnicorn': todoText
+async function undo(){//Beginning the async function called undo().
+    const todoText = this.parentNode.childNodes[1].innerText//declaring the todoText variable. Not sure what any of it breaks down to be except for the 'innerText' bit.
+    try{//beginning of the try/catch function. Will happen first before the catch bit.
+        const response = await fetch('undo', {//Creating the response variable to be equal to waiting for the data in undo to be fetched.
+            method: 'put',//declaring that the method is put as opposed to the other methods in CRUD.
+            headers: {'Content-type': 'application/json'},//Declaring the headers to be JSON. Not sure what 'content-type' is and what could be put in its place.
+            body: JSON.stringify({//Putting the JSON into a string?
+                'rainbowUnicorn': todoText//making 'rainbowUnicorn' equal to todoText.
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+        const data = await response.json()//Declaring data to wait for the JSON response.
+        console.log(data)//Console logging the data variable.
+        location.reload()//Reloading the page? Not sure what location is.
+    }catch(err){//second part of the try/catch function. Runs if there is an error.
+        console.log(err)//Console logging the error if there is one.
     }
 }
--- server.js
+++ server.js
@@ -1,74 +1,74 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') //Makes a variable to store express in. 
+const app = express() //Makes a variable that implements express
+const MongoClient = require('mongodb').MongoClient //Makes a variable to store MongoDB in.
+const PORT = 2121 //Assigns a port number to the PORT variable, which is where your app will be hosted.
+require('dotenv').config() //I don't know what this does. I think it's an additional NPM.
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+let db, //creating db as a variable; no value yet.
+    dbConnectionStr = process.env.DB_STRING, //assigning dbConnectionStr a value. I don't understand what that value breaks down to be. 
+    dbName = 'todo' //Assigning the name of the database to 'todo'
 
-MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
-    .then(client => {
-        console.log(`Hey, connected to ${dbName} database`)
-        db = client.db(dbName)
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true}) //connecting the MongoClient to the dbConnectionStr. I don't know what unifiedTopology is.
+    .then(client => { //.then I think is a function attached to APIs. So it's doing something in regards to that here. 
+        console.log(`Hey, connected to ${dbName} database`) //Setting up a console log notificaiton telling you what database you're connected to.
+        db = client.db(dbName) //finally assigning that db value that was declared earlier. Not sure how 'client' plays into it or how the value is broken down. 
     })
-    .catch(err =>{
-        console.log(err)
+    .catch(err =>{ //Catch function where it talks about IF there's an error...
+        console.log(err)//console log that there's an error. 
     })
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs')//I think that this makes it so we can use ejs, but not sure. 
+app.use(express.static('public')) //I think that this lets us use our public folder, but also not sure. 
+app.use(express.urlencoded({ extended: true }))//I don't know what this is.
+app.use(express.json())//Lets use use json in Express.
 
-app.get('/', async (req,res)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    res.render('index.ejs', {zebra: todoItems, left: itemsLeft})
+app.get('/', async (req,res)=>{//Reading and taking us to the base homepage (/). Also setting up an async/await function. 
+    const todoItems = await db.collection('todos').find().toArray()//assigning todoItems the value of waiting for the database collection to find its values and put them into an array. 
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false})//assigning itemsLeft to wait for the database collection of todos to count documents. Not sure what the value of false has to do with this here.
+    res.render('index.ejs', {zebra: todoItems, left: itemsLeft})//Renders the index.ejs doc. Not sure what the zebra:todoItems and left:itemsLeft do.
 })
 
-app.post('/createTodo', (req, res)=>{
-    db.collection('todos').insertOne({todo: req.body.todoItem, completed: false})
-    .then(result =>{
-        console.log('Todo has been added!')
-        res.redirect('/')
+app.post('/createTodo', (req, res)=>{//starts the post() function and sets the destination of the page to be /createTodo.
+    db.collection('todos').insertOne({todo: req.body.todoItem, completed: false})//Inserts an uncompleted todo item to the database collection.
+    .then(result =>{//.then() function called. Probably means what will happen after the condition above is fulfilled. 
+        console.log('Todo has been added!')//console logs that a todo item has been added. 
+        res.redirect('/')//redirects to main page.
     })
 })
 
-app.put('/markComplete', (req, res)=>{
-    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
-        $set: {
-            completed: true
+app.put('/markComplete', (req, res)=>{//starts the put() function. I think this is the Update in CRUD. Sets the destination page to be /markComplete.
+    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{//Updates the todos in the database. I don't know what req.body.rainbowUnicorn is/does.
+        $set: { //I don't know why it looks like $set, but this is setting the boolean value of the todo item.
+            completed: true//Setting the boolean value of the todo item.
         }
     })
-    .then(result =>{
-        console.log('Marked Complete')
-        res.json('Marked Complete')
+    .then(result =>{//taking the consequent value of the result
+        console.log('Marked Complete')//console logging that the item is marked complete. 
+        res.json('Marked Complete')//Inputting to the JSOn that the item is marked complete. 
     })
 })
 
-app.put('/undo', (req, res)=>{
-    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
-        $set: {
-            completed: false
+app.put('/undo', (req, res)=>{//For updating undoing whether a function is completed or not; destination is /undo.
+    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{//Updating one of the todo items
+        $set: {//Setting the boolean value of the todo item.
+            completed: false//Setting the boolean value of 'completed' to be false. 
         }
     })
-    .then(result =>{
-        console.log('Marked Complete')
-        res.json('Marked Complete')
+    .then(result =>{//Taking the result of the above
+        console.log('Marked Complete')//console logging that the item is marked complete. 
+        res.json('Marked Complete')//Inputting to the JSOn that the item is marked complete.
     })
 })
 
-app.delete('/deleteTodo', (req, res)=>{
-    db.collection('todos').deleteOne({todo:req.body.rainbowUnicorn})
-    .then(result =>{
-        console.log('Deleted Todo')
-        res.json('Deleted It')
+app.delete('/deleteTodo', (req, res)=>{//Delete step of CRUD. Starting the delete() function. Destination is /deleteTodo.
+    db.collection('todos').deleteOne({todo:req.body.rainbowUnicorn})//Deleting one of the todo items.
+    .then(result =>{//taking the result and
+        console.log('Deleted Todo')//console logging that the item is deleted.
+        res.json('Deleted It')//Inputting to the JSOn that the item is deleted.
     })
-    .catch( err => console.log(err))
+    .catch( err => console.log(err))//catch() function catching any of the errors. If there are any, console logging the error. 
 })
  
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log('Server is running, you better catch it!')
+app.listen(process.env.PORT || PORT, ()=>{//Making the server listen on the determined PORT or whatever PORT MongoDB assigns to it. 
+    console.log('Server is running, you better catch it!')//Console logging that the server is up and running.
 })    
