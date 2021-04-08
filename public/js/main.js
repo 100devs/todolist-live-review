e0b098221ea3de9591a35ed5161284fd3ff5cd4d
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-04-07 22:21:17 -0400

    added comments to main.js, ejs and css

--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,105 @@
+//dynamically adding eventlisteners to each task as they appear within the EJS li. 
+
+//selects all items in ejs with the specified class
 const deleteBtn = document.querySelectorAll('.del')
 const todoItem = document.querySelectorAll('.todoItem span')
 const todoComplete = document.querySelectorAll('.todoItem span.completed')
 
-Array.from(deleteBtn).forEach((el)=>{
+//creates arrays from items above, then loops through them adding event listeners and pairing a function to each
+Array.from(deleteBtn).forEach((el) => {
     el.addEventListener('click', deleteTodo)
 })
 
-Array.from(todoItem).forEach((el)=>{
+Array.from(todoItem).forEach((el) => {
     el.addEventListener('click', markComplete)
 })
 
-Array.from(todoComplete).forEach((el)=>{
+Array.from(todoComplete).forEach((el) => {
     el.addEventListener('click', undo)
 })
 
-async function deleteTodo(){
+//asynchronous function, that allows deletion of tasks
+async function deleteTodo() {
+
+    //todoText refers us to the inner text of the li node with the specified index number of 1 -- its not zero because spaces count
+    //I don't know what "this" is pointing to though
     const todoText = this.parentNode.childNodes[1].innerText
-    try{
+
+    //try this, if not return an error and log it to the console
+    try {
+        //a fetch is made to our server.js file, for the function with the delete method
         const response = await fetch('deleteTodo', {
             method: 'delete',
-            headers: {'Content-type': 'application/json'},
+
+            //I DONT KNOW
+            headers: { 'Content-type': 'application/json' },
+
+            //I DONT KNOW
             body: JSON.stringify({
+                //I DONT KNOW
                 'rainbowUnicorn': todoText
             })
         })
+        //create a variable named data, that stores the response in json format, then log it to the console and refresh the page
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function markComplete(){
+
+//async function that allows us to use the put method to update our list and mark a task as complete
+async function markComplete() {
+    //todoText refers us to the inner text of the li node with the specified index number of 1 -- its not zero because spaces count
+    //I DONT KNOW what "this" is pointing to 
     const todoText = this.parentNode.childNodes[1].innerText
-    try{
+    try {
+        // make a fetch for a put method function in our server.js that corresponds to the make markComplete
         const response = await fetch('markComplete', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            //I DONT KNOW
+            headers: { 'Content-type': 'application/json' },
+            //I DONT KNOW
             body: JSON.stringify({
                 'rainbowUnicorn': todoText
             })
         })
+
+        //create a variable named data, that stores the response in json format, then log it to the console and refresh the page
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
 
-async function undo(){
+// asyn function that allows user to undo what the above function does
+async function undo() {
+
+    //todoText refers us to the inner text of the li node with the specified index number of 1 -- its not zero because spaces count
+    //I DONT KNOW what "this" is pointing to 
     const todoText = this.parentNode.childNodes[1].innerText
-    try{
+    try {
+
+        // make a fetch for a put method function in our server.js that corresponds to the make markComplete
         const response = await fetch('undo', {
             method: 'put',
-            headers: {'Content-type': 'application/json'},
+            //I DONT KNOW
+            headers: { 'Content-type': 'application/json' },
+            //I DONT KNOW
             body: JSON.stringify({
                 'rainbowUnicorn': todoText
             })
         })
+
+        //create a variable named data, that stores the response in json format, then log it to the console and refresh the page
         const data = await response.json()
         console.log(data)
         location.reload()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 }
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
+//we set an express variable to require express
 const express = require('express')
+//ser an app variable that uses the express functionalities
 const app = express()
+//set a mongo variable that utilizes mondo db functionality
 const MongoClient = require('mongodb').MongoClient
+//set a port variable with out chosen port number
 const PORT = 2121
+//this allows us to use the dotenv functionality, which keeps important items private, make sure to put env file in git ignore so it does not get pushed to github
 require('dotenv').config()
 
+// create a variable that stores a path to link to our database
+//I DONT KNOW  why we use a comma after db
 let db,
+    //db string is our url for the database. this is hidden in our dot env
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
-MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Hey, connected to ${dbName} database`)
         db = client.db(dbName)
     })
-    .catch(err =>{
+    .catch(err => {
         console.log(err)
     })
 
@@ -22,53 +31,53 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/', async (req,res)=>{
+app.get('/', async (req, res) => {
     const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    res.render('index.ejs', {zebra: todoItems, left: itemsLeft})
+    const itemsLeft = await db.collection('todos').countDocuments({ completed: false })
+    res.render('index.ejs', { zebra: todoItems, left: itemsLeft })
 })
 
-app.post('/createTodo', (req, res)=>{
-    db.collection('todos').insertOne({todo: req.body.todoItem, completed: false})
-    .then(result =>{
-        console.log('Todo has been added!')
-        res.redirect('/')
-    })
+app.post('/createTodo', (req, res) => {
+    db.collection('todos').insertOne({ todo: req.body.todoItem, completed: false })
+        .then(result => {
+            console.log('Todo has been added!')
+            res.redirect('/')
+        })
 })
 
-app.put('/markComplete', (req, res)=>{
-    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
+app.put('/markComplete', (req, res) => {
+    db.collection('todos').updateOne({ todo: req.body.rainbowUnicorn }, {
         $set: {
             completed: true
         }
     })
-    .then(result =>{
-        console.log('Marked Complete')
-        res.json('Marked Complete')
-    })
+        .then(result => {
+            console.log('Marked Complete')
+            res.json('Marked Complete')
+        })
 })
 
-app.put('/undo', (req, res)=>{
-    db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
+app.put('/undo', (req, res) => {
+    db.collection('todos').updateOne({ todo: req.body.rainbowUnicorn }, {
         $set: {
             completed: false
         }
     })
-    .then(result =>{
-        console.log('Marked Complete')
-        res.json('Marked Complete')
-    })
+        .then(result => {
+            console.log('Marked Complete')
+            res.json('Marked Complete')
+        })
 })
 
-app.delete('/deleteTodo', (req, res)=>{
-    db.collection('todos').deleteOne({todo:req.body.rainbowUnicorn})
-    .then(result =>{
-        console.log('Deleted Todo')
-        res.json('Deleted It')
-    })
-    .catch( err => console.log(err))
+app.delete('/deleteTodo', (req, res) => {
+    db.collection('todos').deleteOne({ todo: req.body.rainbowUnicorn })
+        .then(result => {
+            console.log('Deleted Todo')
+            res.json('Deleted It')
+        })
+        .catch(err => console.log(err))
 })
- 
-app.listen(process.env.PORT || PORT, ()=>{
+
+app.listen(process.env.PORT || PORT, () => {
     console.log('Server is running, you better catch it!')
-})    
+})
