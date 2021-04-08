382b2fbcbe662073da6c0e0abb6032be9b9be8b3
JessieTMossa <76855680+JessieTMossa@users.noreply.github.com>
2021-04-08 05:47:27 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,69 @@
-const deleteBtn = document.querySelectorAll('.del')
-const todoItem = document.querySelectorAll('.todoItem span')
-const todoComplete = document.querySelectorAll('.todoItem span.completed')
+const deleteBtn = document.querySelectorAll('.del') //constant variable for delete function. I don't understand the All part of the querySelector
+const todoItem = document.querySelectorAll('.todoItem span') //constant variable for edit function (to mark complete I think). I don't understand the All part of the querySelector
+const todoComplete = document.querySelectorAll('.todoItem span.completed') //constant variable for edit function (to undo a complete I think). I don't understand the All part of the querySelector
 
-Array.from(deleteBtn).forEach((el)=>{
+Array.from(deleteBtn).forEach((el)=>{ //Array generated from all deletes, then iterates through each element to add an event listener for the delete function
     el.addEventListener('click', deleteTodo)
 })
 
-Array.from(todoItem).forEach((el)=>{
+Array.from(todoItem).forEach((el)=>{ //Array generated from all items, then iterates through each element to add an event listener for the complete function
     el.addEventListener('click', markComplete)
 })
 
-Array.from(todoComplete).forEach((el)=>{
+Array.from(todoComplete).forEach((el)=>{ //Array generated from all completed items, then iterates through each element to add an event listener for the undo function
     el.addEventListener('click', undo)
 })
 
-async function deleteTodo(){
-    const todoText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteTodo', {
-            method: 'delete',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'rainbowUnicorn': todoText
+async function deleteTodo(){ //asynchronous function to delete a todo item
+    const todoText = this.parentNode.childNodes[1].innerText //I know this gives us the todo item text, but I don't understand how
+    try{ //I think this is the "if" part of the async, if it fails it goes to the catch section
+        const response = await fetch('deleteTodo', { //constant variable, I don't know what the rest is
+            method: 'delete', //I don't know what this is
+            headers: {'Content-type': 'application/json'}, //I don't know what this is
+            body: JSON.stringify({ //I don't know what this is
+                'rainbowUnicorn': todoText //I don't know what this is
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+        const data = await response.json() //I don't know what this is
+        console.log(data) //print the value of the data variable to the console
+        location.reload() //I don't know what this is
+    }catch(err){ //I think this is the "else" part of the async function, but don't understand it
+        console.log(err) //print error to the console
     }
 }
 
-async function markComplete(){
-    const todoText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'rainbowUnicorn': todoText
+async function markComplete(){ //asynchronous function to mark a todo item complete
+    const todoText = this.parentNode.childNodes[1].innerText //I know this gives us the todo item text, but I don't understand how
+    try{ //I think this is the "if" part of the async, if it fails it goes to the catch section
+        const response = await fetch('markComplete', { //constant variable, I don't know what the rest is
+            method: 'put', //I don't know what this is
+            headers: {'Content-type': 'application/json'}, //I don't know what this is
+            body: JSON.stringify({ //I don't know what this is
+                'rainbowUnicorn': todoText //I don't know what this is
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+        const data = await response.json() //I don't know what this is
+        console.log(data) //print the value of the data variable to the console
+        location.reload() //I don't know what this is
+    }catch(err){ //I think this is the "else" part of the async function, but don't understand it
+        console.log(err) //print error to the console
     }
 }
 
-async function undo(){
-    const todoText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('undo', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'rainbowUnicorn': todoText
+async function undo(){ //asynchronous function to unmark a completed todo item
+    const todoText = this.parentNode.childNodes[1].innerText //I know this gives us the todo item text, but I don't understand how
+    try{ //I think this is the "if" part of the async, if it fails it goes to the catch section
+        const response = await fetch('undo', { //constant variable, I don't know what the rest is
+            method: 'put', //I don't know what this is
+            headers: {'Content-type': 'application/json'}, //I don't know what this is
+            body: JSON.stringify({ //I don't know what this is
+                'rainbowUnicorn': todoText //I don't know what this is
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
+        const data = await response.json() //I don't know what this is
+        console.log(data) //print the value of the data variable to the console
+        location.reload() //I don't know what this is
+    }catch(err){ //I think this is the "else" part of the async function, but don't understand it
+        console.log(err) //print error to the console
     }
 }
