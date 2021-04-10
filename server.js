64ec24ef75a18c34ad9583d8dfbf10d451d9cb64
James Cababa <jimmycababa@gmail.com>
2021-04-10 15:22:28 -0500

    todolist-cababa

--- server.js
+++ server.js
@@ -1,41 +1,69 @@
-const express = require('express')
+// installing and using the dependencies
+const express = require('express') 
+// takes express and running it (can use app.)
 const app = express()
+// connecting to the database
 const MongoClient = require('mongodb').MongoClient
+// port we are hosting on
 const PORT = 2121
 require('dotenv').config()
 
+// name of database and the connection url from mondodb
 let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+dbConnectionStr = process.env.DB_STRING,
+dbName = 'todo'
 
+// useUnifiedTopology is to prevent errors when building our project
+// connect takes in the connection string and another object(unified thing)
+// this whole thing is a prompt (promise) and once its connected to the db then the .then runs
+// inside the .then is our connection to the client(db)
+// storing the connection in the db variable(client.db(dbName)
 MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
     .then(client => {
         console.log(`Hey, connected to ${dbName} database`)
         db = client.db(dbName)
     })
-    .catch(err =>{
+// the catch that handles the promise
+    .catch(err => {
         console.log(err)
     })
 
+    // setting up the server and everything it needs to do its job
+    // application is set up to use the ejs file
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+// server can serve up whatever is in the public folder
+app.use(express.static('public')) 
+// to look at the application, look at req and pull data
+app.use(express.urlencoded({ extended: true})) 
+// to look at the application, look at req and pull data
 app.use(express.json())
 
-app.get('/', async (req,res)=>{
+// getting the req from the user and connecting ejs
+// find will find all the documents in the collection
+// this whole thing is a promise so it needs a .then
+// zebra is the array of objects
+app.get('/', async (req, res) => {
     const todoItems = await db.collection('todos').find().toArray()
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
     res.render('index.ejs', {zebra: todoItems, left: itemsLeft})
 })
 
-app.post('/createTodo', (req, res)=>{
+// talking to the database through this route(app.post)
+// createTodo is from the action form in our ejs
+// need req and res to handle the request and response in order for us to do anything
+// we are then pushing to the database
+// insertOne enables us to insert a document into the db collection (will take in two things (information from the user from the form))
+// completed false that is a property that will always be false (because its a todo item that should not have a complete status) 
+app.post('/createTodo', (req, res) => {
     db.collection('todos').insertOne({todo: req.body.todoItem, completed: false})
-    .then(result =>{
-        console.log('Todo has been added!')
+    .then(result => {
+        console.log('Todo has been added')
         res.redirect('/')
     })
 })
 
+// updateOne methods takes in a bunch of stuff. first thing it takes in is the match (todo:)
+// once it finds a match, $set it to true
 app.put('/markComplete', (req, res)=>{
     db.collection('todos').updateOne({todo: req.body.rainbowUnicorn},{
         $set: {
@@ -60,6 +88,8 @@ app.put('/undo', (req, res)=>{
     })
 })
 
+// deleteTodo is from the fetch so now they connect
+// deleteOne is trying to find a match with all the todo properties
 app.delete('/deleteTodo', (req, res)=>{
     db.collection('todos').deleteOne({todo:req.body.rainbowUnicorn})
     .then(result =>{
@@ -68,7 +98,8 @@ app.delete('/deleteTodo', (req, res)=>{
     })
     .catch( err => console.log(err))
 })
- 
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+
+// the way to start our server. app.listen takes in PORT
+    app.listen(process.env.PORT || PORT, () => {
+        console.log('running')
+    })
