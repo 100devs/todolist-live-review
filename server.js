9bfa4c4661c207ce42af17173579dbb4285cee10
Bryce Stabenow <brycestabenow617@gmail.com>
2022-04-24 14:14:50 -0700

    testing fork

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
 require('dotenv').config()
+const pineapple = "pineapple";
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
